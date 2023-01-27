ef6a06958df27a52fbd3cc6771cb9c9a0c814153
AB <anitabanhtx@gmail.com>
2023-01-27 16:37:54 -0600

    created Intern and Manager subclasses, functions for intern and engineer prompts, added emojis to rolesrendered on card.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const generateHTML = require("./utils/generateHTML");
+const Manager = require("./lib/Manager");
 
-inquirer
-  .prompt([
+//create team function Which type of team member would you like to add?
+const teamMembers = [];
+const idArray = [];
+
+// wraps the entire including html function  --- function menu() {}
+// call function menu ();
+
+function createManager() {
+  inquirer.prompt([
     {
       type: "input",
-      message: "What is the employee name?",
+      message: "What is the team manager name?",
       name: "name",
     },
     {
@@ -19,49 +27,138 @@ inquirer
       message: "What is the employee email address?",
       name: "email",
     },
-    //manager only question
     {
       type: "input",
       message: "What is your office phone number?",
-      name: "phone",
-    },
-    //how do I add the option to add an Eng/Intern? A separate .prompt? 
-    // {
-    //     type: "list",
-    //     message: "What is the employee's role?",
-    //     name: "role",
-    //     choices: ["Engineer", "Intern"],
-    //   },
-    // {
-    //   type: "input",
-    //   message: "What is the employee name?",
-    //   name: "name",
-    // },
-    //   //for Engineer only
-    //   {
-    //     type: "input",
-    //     message: "What is your GitHub URL?",
-    //     name: "GitHub",
-    //   },
-    // //for Intern only
-    // {
-    //   type: "input",
-    //   message: "What school is the intern attending?",
-    //   name: "school",
-    // },
+      name: "officeNum",
+    }
   ])
-  
+  .then((answers) => {
+    console.log('creating manager');
+    const manager = new Manager(...answers);
+    teamMembers.push(manager);
+    idArray.push(answers.id);
+    console.table(teamMembers);
+}).catch((err) => console.error(err));
+}
+ 
+function createEngineer() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is your Engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your GitHub URL?",
+        name: "gitHub",
+        }
+        ]).then((answers) => {
+            console.log('creating engineer')
+        })
+}
+
+function createIntern() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your intern's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What school does your intern attend?",
+        name: "school",
+        }
+        ]).then((answers) => {
+            console.log('creating engineer')
+        })
+}
+
+
+function mainPrompt() {
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "What type of employee would you like to add?",
+        name: "choice",
+        choices: ["Engineer", "Intern", "none"],
+      }  
+    ]).then(({choice}) => {
+        console.log(choice);
+        switch (choice) {
+            case "Engineer":
+                createEngineer();
+                console.log('creating New Engineer');
+                break;
+            case "Intern":
+                createIntern();
+                console.log('creating Intern');
+                break;
+            default:
+                console.log('none selected')
+        }
+    })
+}
+
+//how do I add the option to add an Eng/Intern? A separate .prompt?
+// {
+//     type: "list",
+//     message: "What is the employee's role?",
+//     name: "role",
+//     choices: ["Engineer", "Intern"],
+//   },
+// {
+//   type: "input",
+//   message: "What is the employee name?",
+//   name: "name",
+// },
+//   //for Engineer only
+//  
+// //for Intern only
+// {
+//   type: "input",
+//   message: "What school is the intern attending?",
+//   name: "school",
+// },
 
 //function to write HTML file
 function writeToFile(fileName, data) {
-    var htmlString = generateHTML(data);
-    fs.writeFile(fileName, htmlString, (err) => {
-     return err ? console.log(err) : console.log("Directory was sucessfully created.");
-    });    
+  var htmlString = generateHTML(data);
+  fs.writeFile(fileName, htmlString, (err) => {
+    return err
+      ? console.log(err)
+      : console.log("Directory was sucessfully created.");
+  });
 }
 
-//initialize application
-function init() {
-  questions().then((answers) => writeToFile("./dist/list.HTML", answers));}
 
-  init();
+
+//initialize application update createManager later 
+// fix to run create manager first, then run main prompt and create the file
+function init() {
+  createManager().then(() => {
+  mainPrompt().then((answers) => writeToFile("./dist/list.HTML", answers));
+  } )
+}
+
+init();
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,11 +1,11 @@
 //Create function that renders school name if intern is selected, github if Engineer is selected
-function renderVariableInfo (data) {
-if(data.role == Manager) {
-    return `Office number ${data.phone}`;}
-    if(data.role == Engineer) {
-   return `GitHub URL:${data.GitHub}`
+function renderVariableCardInfo (data) {
+if(data.role == "Manager") {
+    return `<li class="list-group-item">Office number ${data.phone}</li>`;}
+    if(data.role == "Engineer") {
+   return `<li class="list-group-item">GitHub URL:${data.GitHub}</li>`
     }
-    if(data.role == Intern) {
+    if(data.role == "Intern") {
      return `School: ${data.school}`
     }
 }
@@ -14,35 +14,36 @@ if(data.role == Manager) {
 //Create function that populates the role subtitle. 
     // ADD emoji coffee, glasses
 function renderRole (data) {
-    if(data.role == Manager) {
-        return `Manager`;}
-        if(data.role == Engineer) {
-       return `Engineer`
+    if(data.role == "Manager") {
+        return `☕ Manager`;}
+        if(data.role == "Engineer") {
+       return `💻 Engineer`
         }
-        if(data.role == Intern) {
-         return `Intern`
+        if(data.role == "Intern") {
+         return `👨‍🎓 Intern`
         }
     }
 
+//ADD link to style.CSS
 function generateHTML(data) {
     return `<!doctype html>
         <html lang="en">
           <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Portfolio</title>
+            <title>Team Portfolio</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
           </head>
           <body>
           
           <div class="card">
           <h5 class="card-header">${data.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${data.role}</h6>
+          <h6 class="card-subtitle mb-2 text-muted">${renderRole}</h6>
           <div class="card-body">
           <ul class="list-group list-group-flush">
           <li class="list-group-item">ID: ${data.id}</li> 
           <li class="list-group-item">Email: ${data.email}</li>
-          <li class="list-group-item">${renderVariableInfo}</li>
+          ${renderVariableCardInfo}
         </ul>
           </div>
         </div>
