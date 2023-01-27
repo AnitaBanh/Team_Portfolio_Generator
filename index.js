e8b0cb431b7d1daf8c58ead19134839171e76e5e
AB <anitabanhtx@gmail.com>
2023-01-27 13:42:34 -0600

    added prompt for role

--- index.js
+++ index.js
@@ -6,43 +6,49 @@ inquirer
   .prompt([
     {
       type: "input",
-      message: "What is the team manager name?",
-      name: "manager",
+      message: "What is the employee name?",
+      name: "name",
     },
     {
       type: "input",
-      message: "What is your employee ID?",
+      message: "What is the employee ID?",
       name: "id",
     },
     {
       type: "input",
-      message: "What is your email address?",
+      message: "What is the employee email address?",
       name: "email",
     },
+    //manager only question
     {
       type: "input",
       message: "What is your office phone number?",
       name: "phone",
     },
     //how do I add the option to add an Eng/Intern? A separate .prompt? 
-
-    {
-      type: "input",
-      message: "What is the employee name?",
-      name: "name",
-    },
-      //for Engineer only
-      {
-        type: "input",
-        message: "What is your GitHub URL?",
-        name: "GitHub",
-      },
-    //for Intern only
-    {
-      type: "input",
-      message: "What school is the intern attending?",
-      name: "school",
-    },
+    // {
+    //     type: "list",
+    //     message: "What is the employee's role?",
+    //     name: "role",
+    //     choices: ["Engineer", "Intern"],
+    //   },
+    // {
+    //   type: "input",
+    //   message: "What is the employee name?",
+    //   name: "name",
+    // },
+    //   //for Engineer only
+    //   {
+    //     type: "input",
+    //     message: "What is your GitHub URL?",
+    //     name: "GitHub",
+    //   },
+    // //for Intern only
+    // {
+    //   type: "input",
+    //   message: "What school is the intern attending?",
+    //   name: "school",
+    // },
   ])
   
 
